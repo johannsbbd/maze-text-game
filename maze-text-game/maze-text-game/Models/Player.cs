30a7b97113b9cc17b75850eb5dd06c371b467566
schej002 <johannes.schepers@vcontractor.co.za>
2022-06-12 15:41:56 +0200

    Implemented Player model record.

--- maze-text-game/maze-text-game/Models/Player.cs
+++ maze-text-game/maze-text-game/Models/Player.cs
@@ -1,6 +1,12 @@
 ﻿namespace maze_text_game
 {
-    public class Player
+    public record Player
     {
+        public string Name;
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
     }
 }
