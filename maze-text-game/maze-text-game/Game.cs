61b572d635ef2c2ecb8e71dcd772cbf72b47c31a
schej002 <johannes.schepers@vcontractor.co.za>
2022-06-12 15:16:12 +0200

    GameException and SessionId property.

--- maze-text-game/maze-text-game/Game.cs
+++ maze-text-game/maze-text-game/Game.cs
@@ -6,16 +6,16 @@ namespace maze_text_game
 {
     public class Game
     {
-        private string sessionId;
+        public string SessionId { get; private set; }
         private Map map;
         private int playerLimit;
         private Dictionary<string, Player> players;
         private Player victoriousPlayer = null;
         private Dictionary<string, Point> playerPositions;
 
-        public Game(int playerLimit, Size mapSize) { 
+        public Game(int playerLimit, Size mapSize) {
             this.playerLimit = playerLimit;
-            this.sessionId = Guid.NewGuid().ToString();
+            this.SessionId = Guid.NewGuid().ToString();
             this.map = new Map(mapSize.Height, mapSize.Width);
             this.players = new Dictionary<string, Player>();
             this.playerPositions = new Dictionary<string, Point>();
@@ -26,16 +26,20 @@ namespace maze_text_game
         }
     }
 
-    public enum Direction { 
-        North, 
-        East, 
-        South, 
+    public enum Direction {
+        North,
+        East,
+        South,
         West,
     }
 
-    public enum State { 
+    public enum State {
         Waiting,
         InProgress,
         Ended,
+    }
+
+    public class GameException : Exception {
+        GameException(string msg) : base(msg) { }   
     }
 }
