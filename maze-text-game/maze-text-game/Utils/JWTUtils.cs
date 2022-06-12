d50154a8ffd2dc50305cc094740f77ead03678ec
schej002 <johannes.schepers@vcontractor.co.za>
2022-06-12 17:00:44 +0200

    Added LogUtils and Stub JWTUtils.

---
+++ maze-text-game/maze-text-game/Utils/JWTUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace maze_text_game.Utils
+{
+    public static class JWTUtils
+    {
+
+        public static bool VerifyToken(string token)
+        {
+            return false;
+        }
+
+        public static string GetPlayerGuid(string token)
+        {
+            return "";
+        }
+
+        public static string GetPlayerName(string token)
+        {
+            return "";
+        }
+    }
+}
