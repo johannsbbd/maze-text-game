18c4a4e9685904a0d90cb5da7fdfd192e8c758ec
Sean Groenewald <seang@bbd.co.za>
2022-06-14 14:54:34 +0200

    Created console client for maze game

---
+++ maze-text-game/console-client/Api.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_client
+{
+    internal class Api
+    {
+
+        static string apiBase = "https://localhost:5001";
+        
+        static string token = null;
+
+        public static bool Auth(string name) {
+            WebRequest req = WebRequest.Create($"{apiBase}/auth");
+            req.Method = "POST";
+
+            AddHeader(req);
+            AddBody(req, new { 
+                PlayerName = name,
+            });
+
+            var response = (HttpWebResponse) req.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK) {
+                dynamic obj = ReadResponseStream(response.GetResponseStream());
+                token = obj.Token;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool RunCommand(string gameId, string cmd) {
+            WebRequest req = WebRequest.Create($"{apiBase}/Command");
+            req.Method = "POST";
+
+            AddHeader(req);
+            AddBody(req, new {
+                GameGuid = gameId,
+                Command = cmd,
+            });
+
+            try
+            {
+                var response = (HttpWebResponse)req.GetResponse();
+                return response.StatusCode == HttpStatusCode.OK;
+            }
+            catch {
+                return false;
+            }            
+        }
+
+        public static dynamic CreateGame(int playerLimit, int mapWidth, int mapHeight) {
+            WebRequest req = WebRequest.Create($"{apiBase}/Game/create");
+            req.Method = "POST";
+            
+            AddHeader(req);
+            AddBody(req, new {
+                PlayerLimit = playerLimit,
+                MapWidth = mapWidth,
+                MapHeight = mapHeight,
+            });
+
+            dynamic obj = ReadResponseStream(req.GetResponse().GetResponseStream());
+            return obj;
+        }
+
+        public static bool JoinGame(string gameId) {
+            WebRequest req = WebRequest.Create($"{apiBase}/Game/join");
+            req.Method = "POST";
+
+            AddHeader(req);
+            AddBody(req, new {
+                GameGuid = gameId,
+            });
+
+            var response = req.GetResponse();
+            
+            return ((HttpWebResponse) response).StatusCode == HttpStatusCode.OK;
+        }
+
+        public static bool VoteStart(string gameId) {
+            WebRequest req = WebRequest.Create($"{apiBase}/Game/vote_start");
+            req.Method= "POST";
+
+            AddHeader(req);
+            AddBody(req, new
+            {
+                GameGuid = gameId,
+            });
+
+            return ((HttpWebResponse)req.GetResponse()).StatusCode == HttpStatusCode.OK;
+        }
+
+        public static dynamic GetGame(string gameId) {
+            WebRequest req = WebRequest.Create($"{apiBase}/Game?gameId={gameId}");
+            req.Method = "GET";
+
+            AddHeader(req);
+
+            dynamic obj = ReadResponseStream(req.GetResponse().GetResponseStream());
+            return obj;
+        }
+
+        private static dynamic ReadResponseStream(Stream responseStream) {
+            using (StreamReader reader = new StreamReader(responseStream)) {
+                return JsonConvert.DeserializeObject(reader.ReadToEnd());
+            }            
+        }
+
+        private static void AddHeader(WebRequest req) { 
+            req.Headers.Add("Authorization", $"Bearer {token}");
+        }
+
+        private static void AddBody(WebRequest req, object body) {
+            req.ContentType = "application/json";
+
+            using (var streamWriter = new StreamWriter(req.GetRequestStream())) {
+                string json = System.Text.Json.JsonSerializer.Serialize(body);
+
+                streamWriter.Write(json);
+            }
+        }
+    }
+}
---
+++ maze-text-game/console-client/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace console_client
+{
+    internal class Program
+    {
+
+        static string currentGameId = null;
+
+        static Thread renderThread = new Thread(() => {
+            while (currentGameId != null) {
+                Thread.Sleep(5000);
+                RenderActiveGame();
+            }
+        });
+
+        static void RenderActiveGame() {
+            Console.Clear();
+            dynamic game = Api.GetGame(currentGameId);
+            Console.WriteLine(game.GameState);
+            Console.WriteLine(game.RenderedMap);
+        }
+
+        static void Main(string[] args)
+        {
+            bool ended = false;
+
+            while (!ended) {
+                string input = Console.ReadLine();
+
+                if (input == "") {
+                    break;
+                }
+
+                if (Regex.IsMatch(input, @"create \d+\s\d+\s\d+")) {
+                    if (currentGameId != null) {
+                        Console.WriteLine("You are already in a game");
+                        continue;
+                    }
+
+                    string[] tokens = input.Split(' ');
+
+                    string guid = Api.CreateGame(Convert.ToInt32(tokens[1]), Convert.ToInt32(tokens[2]), Convert.ToInt32(tokens[3]))?.GameGuid;
+                    if (guid == null) {
+                        Console.WriteLine("Failed to create game.");
+                        continue;
+                    }
+
+                    Console.WriteLine(guid);
+                    currentGameId = guid;
+                    RenderActiveGame();
+                    continue;
+                }
+
+                if (Regex.IsMatch(input, @"join .+")) {
+                    if (currentGameId != null) {
+                        Console.WriteLine("You are already in a game");
+                        continue;
+                    }
+
+                    string[] tokens = input.Split(' ');
+
+                    string gameId = tokens[1];
+
+                    if (Api.JoinGame(gameId)) {
+                        Console.WriteLine("Game joined");
+                    } else {
+                        Console.WriteLine("Could not join game");
+                    }
+
+                    currentGameId = gameId;
+                    RenderActiveGame();
+                    continue;
+                }
+
+                if (Regex.IsMatch(input, @"register .+")) {
+                    string[] tokens = input.Split(' ');
+
+                    if (Api.Auth(tokens[1]))
+                    {
+                        currentGameId = null;
+                        Console.WriteLine("Successfully registered");
+                    }
+                    else {
+                        Console.WriteLine("Failed to process registration");
+                    }
+
+                    continue;
+                }
+
+                if (input == "vote") {
+
+                    if (currentGameId == null)
+                    {
+                        Console.WriteLine("You are not in a game");
+                        continue;
+                    }
+
+                    if (Api.VoteStart(currentGameId))
+                    {
+                        Console.WriteLine("Vote successful");
+                    }
+                    else {
+                        Console.WriteLine("Vote failed");
+                    }
+
+                    RenderActiveGame();
+                    renderThread.Start();
+                    continue;
+                }
+
+                if (input == "get") {
+
+                    if (currentGameId == null) {
+                        Console.WriteLine("You are not in a game");
+                        continue;
+                    }
+
+                    RenderActiveGame();
+
+                    continue;
+                }                
+            }
+
+            while (!ended) {
+
+                var key = Console.ReadKey(true);
+                string command = "";
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        command = "north";
+                        break;
+                    case ConsoleKey.RightArrow:
+                        command = "east";
+                        break;
+                    case ConsoleKey.DownArrow:
+                        command = "south";
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        command = "west";
+                        break;
+                }
+
+                if (command != "") {
+                    Api.RunCommand(currentGameId, command);
+                    RenderActiveGame();
+                }
+            }
+        }
+    }
+}
